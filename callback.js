7b1cab2f24ff9ef886881bcbea85c325edf3f883
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-29 14:39:32 +0600

    callback function for pass different function

---
+++ callback.js
@@ -0,0 +1,12 @@
+function ghotok(patri, name){
+   patri(name)
+}
+const patri = function(name){
+    console.log('okay patri paice',name) 
+}
+const patri1 = function(name){
+    console.log('okay patri2 paice',name) 
+}
+
+ghotok(patri,'polash')
+ghotok(patri1,'ahasan')
