d8cc677828b3c190ebeb6292953b4f9600b77f08
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-30 18:47:59 +0600

    array add using dot or spreate operator and array remove to use fillter array method

--- fetch.js
+++ fetch.js
@@ -24,3 +24,36 @@
  const valueFind = Object.values(element);
  console.log(keyFind)
  console.log(valueFind)
+
+ /////////////////////////////////////////////////////////
+
+ const products=[
+    {name:'samsung',brand:'samson', price:12000,color:'black'},
+    {name:'apple',brand:'ios', price:100000,color:'white'},
+    {name:'ewfa',brand:'affas', price:16000,color:'black'},
+    {name:'faesrw',brand:'o9j9', price:18000,color:'red'},
+    {name:'aref',brand:'ajfsio', price:11000,color:'red'},
+    {name:'afse',brand:' faswr', price:15000,color:'red'}
+]
+
+const newData ={
+    name: 'walton', 
+    brand:'oweoi',
+    price:23090,
+    color:'yellow',
+    other:['lsdjfo','fois'],
+    operation:{
+        band:'asofdj',
+        fods:34
+    }
+}
+
+const neww = [...products, newData]
+console.log(neww)
+
+
+const remaining = products.filter(product => product.color!== 'red');
+console.log(remaining)
+
+const newww = [ ...remaining, newData]
+console.log(newww)
