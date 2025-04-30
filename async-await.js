9fd8e88bc00ba0c848c1bf95fa1d94110cad7746
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-30 15:21:58 +0600

    async- await details in normal funtion and arrow function

--- async-await.js
+++ async-await.js
@@ -7,9 +7,42 @@
     .then(data=> console.log(data))
     setTimeout(()=> {
              console.log('i am third')
-    },2000)
+    },1000)
 
     console.log('i am forth')
   }
+  // here's fetch and settimeout or set interval is asyncronous function
 
 loadData()
+
+/////////////////////////////////////////////////////
+
+const loading = async() => {
+try{
+  console.log('this is first')
+  console.log('this is second')
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/comments/1')
+    const data= await response.json();
+    console.log(data)
+
+    //here's fetch bofore await is a ssync function
+    const result = 10+12;
+    console.log(result)
+}
+catch{
+  console.log('customizee error')
+}
+}
+loading()
+
+
+////////////////////////////////////////////////
+
+async function fetchData(){
+  const responsed = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  const data = await responsed.json();
+  console.log(data)
+}
+fetchData()
+
