5bbfb5413c6fed306cf7afbfa73b20037e1994ba
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-30 18:00:56 +0600

    object distructuring method is declared a varible in the exact property or keys

--- distructuring.js
+++ distructuring.js
@@ -1,3 +1,19 @@
 const friends =['polash','habib', 'ahasan']
 const [element3, element2, element5] = friends;
 console.log(element2, element3, element5)
+
+////////////////////////////////////////////////////
+
+const person={
+    name:'shuba',
+    age:23,
+    friendss:['alam','jahan'],
+    otherFriends:{
+        counrty: 'bd',
+        status:'married'
+    }
+}
+
+const {name,age, friendss, otherFriends}= person
+console.log(otherFriends.status)
+console.log(age)
