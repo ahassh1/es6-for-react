1e50f8952e129ac414b3d14299fbf689ac6deb6b
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-01 12:00:35 +0600

    truthy and falsy value defined

--- shortcut.js
+++ shortcut.js
@@ -1,13 +1,44 @@
 //  false value: 0 "" false null undefined ;
 //  truthy value: 'lsfjd' 243 {} [] true;
 
-const test = '';
-if(test){
-    console.log("truthy value")
-}
-else{
-    console.log('false value')
-}
+// const test = '';
+// if(test){
+//     console.log("truthy value")
+// }
+// else{
+//     console.log('false value')
+// }
 
-const testt  = {};
- testt ? console.log('is truthy') : console.log('is falsy')
+// const testt  = {};
+//  testt ? console.log('is truthy') : console.log('is falsy')
+
+
+ //////////////////////////////////////////////////////////////////////
+
+//  const num =50;
+// const result = (num >= 10 && num < 20)? 'true':'false'
+// console.log(result)
+
+// const num = 60;
+// let result;
+// if(num>= 10 && num < 60){
+//     result =true
+// }
+// else{
+    
+//    result = false;
+// }
+// console.log(result)
+
+////////////////////////////////////////////////////////
+
+function totalNum(num){
+ if(num >10 || num <30){
+    return true;
+ }
+ else{
+    return false;
+ }
+}
+ const result = totalNum(60);
+ console.log(result)
