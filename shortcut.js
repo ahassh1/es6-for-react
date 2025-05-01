22b78bb950f06639c1abc2e50866a50abe037cd1
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-01 12:47:48 +0600

    avarage number find in for loop way and function wayy

--- shortcut.js
+++ shortcut.js
@@ -1,47 +1,47 @@
-// //  false value: 0 "" false null undefined ;
-// //  truthy value: 'lsfjd' 243 {} [] true;
+//  false value: 0 "" false null undefined ;
+//  truthy value: 'lsfjd' 243 {} [] true;
 
-// const test = '';
-// if(test){
-//     console.log("truthy value")
-// }
-// else{
-//     console.log('false value')
-// }
+const test = '';
+if(test){
+    console.log("truthy value")
+}
+else{
+    console.log('false value')
+}
 
-// const testt  = {};
-//  testt ? console.log('is truthy') : console.log('is falsy')
+const testt  = {};
+ testt ? console.log('is truthy') : console.log('is falsy')
 
 
-//  //////////////////////////////////////////////////////////////////////
+ //////////////////////////////////////////////////////////////////////
 
-//  const num =50;
-// const result = (num >= 10 && num < 20)? 'true':'false'
-// console.log(result)
-// ////////////////////////////////////////////
-// const num = 60;
-// let result;
-// if(num>= 10 && num < 60){
-//     result =true
-// }
-// else{
+ const num =50;
+const result = (num >= 10 && num < 20)? 'true':'false'
+console.log(result)
+////////////////////////////////////////////
+const num = 60;
+let result;
+if(num>= 10 && num < 60){
+    result =true
+}
+else{
     
-//    result = false;
-// }
-// console.log(result)
+   result = false;
+}
+console.log(result)
 
-// ////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
 
-// function totalNum(num){
-//  if(num >10 || num <30){
-//     return true;
-//  }
-//  else{
-//     return false;
-//  }
-// }
-//  const result = totalNum(60);
-//  console.log(result)
+function totalNum(num){
+ if(num >10 || num <30){
+    return true;
+ }
+ else{
+    return false;
+ }
+}
+ const result = totalNum(60);
+ console.log(result)
 
 
  //////////////////////////////////////////////////////////
@@ -60,7 +60,7 @@
   function totalNums (a,b,c,d,e,f,g){
     const sumNum = a+b+c+d+e+f+g;
     const avg = sumNum /7;
-    const averagee = parseInt(avg)
+    const averagee = parseInt(avg);
     return averagee;
 
   }
